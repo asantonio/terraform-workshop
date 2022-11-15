31e525734d19bce19d4a505e99dde6514b6c48c1
Alastair Antonio <alastair.antonio@gmail.com>
2022-11-15 09:46:37 -0800

    adding variables.tf and cleaning up main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,62 @@
-variable "signalfx_key" {
-  type = string
-}
-
 terraform {
   required_providers {
     signalfx = {
-      source = "splunk-terraform/signalfx"
-      version = "6.1.0"
+      source  = "splunk-terraform/signalfx"
+      version = "6.18.0" # if the version is not included, Terraform will use the latest.
+      # https://registry.terraform.io/providers/splunk-terraform/signalfx/latest/docs
     }
   }
 }
 
 provider "signalfx" {
   auth_token = var.signalfx_key
-  # api_url = "https://api.us0.signalfx.com"
+  api_url    = "https://api.${var.signalfx_realm}.signalfx.com"
 }
 
 # resource "signalfx_time_chart" "mychart0" {
-#     name = "CPU Total Idle"
-#     description = "Very cool chart"
-#
-#     program_text = <<-EOF
-#     data("cpu.total.idle").publish()
+#   name        = "CPU Utilization"
+#   description = "Very cool chart"
+
+#   program_text = <<-EOF
+#     data("cpu.utilization", filter=filter('host', '*')).publish(label='CPU Utilization')
 #     EOF
 # }
-#
+
 # resource "signalfx_dashboard_group" "mydashgroup0" {
-#   name = "Workshop Group"
+#   name        = "Workshop Group - ${var.username}"
+#   description = "${var.username}'s Awesome Workshop Group"
 # }
-#
+
 # resource "signalfx_dashboard" "mydashboard0" {
-#   name = "Workshop Dashboard"
+#   name            = "Workshop Dashboard - ${var.username}"
 #   dashboard_group = signalfx_dashboard_group.mydashgroup0.id
-#
+
 #   chart {
 #     chart_id = signalfx_time_chart.mychart0.id
-#     width = 12
-#     height = 1
-#     row = 0
+#     width    = 6
+#     height   = 2
+#     row      = 0
 #   }
+
+#   chart {
+#     chart_id = signalfx_event_feed_chart.myeventfeed0.id
+#     width    = 3
+#     height   = 3
+#     column = 6
+#   }
+# }
+
+# resource "signalfx_event_feed_chart" "myeventfeed0" {
+#   name        = "Workshop Event Feed - ${var.username}"
+#   description = "Dominos Pizza Order"
+
+#   program_text = <<-EOF
+#     A = events(eventType='dominos_pizza_order:*').publish(label='A')
+#     EOF
+
+# }
+
+# # print URL to dashboard
+# output "dashboard_group_URL" {
+#   value = "https://app.${var.signalfx_realm}.signalfx.com/#/dashboard/${signalfx_dashboard.mydashboard0.id}"
 # }
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "signalfx_key" {
+  type = string
+}
+
+variable "signalfx_realm" {
+  type    = string
+  default = "us1"
+}
+
+variable "username" {
+  type = string
+}
